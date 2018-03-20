25993b978643bee701e36e055b09c44ef0a394b1
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-20 14:00:53 +0200

    Youtube basic search added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import Root from './root/app';
 import reducers from './root/reducer';
 
-const API_KEY = 'AIzaSyD31iFmLH-V4BVq8EDTRH8rY9Xd511eqrQ';
+import Root from './root/app';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <Root />
+    <div>
+      <Root />
+    </div>
   </Provider>
   , document.querySelector('.container'));
--- src/root/app.js
+++ src/root/app.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import YTSearch from 'youtube-api-search';
 
-export default class App extends Component {
+import SearchBar from '../features/search_bar/index';
+
+const API_KEY = 'AIzaSyD31iFmLH-V4BVq8EDTRH8rY9Xd511eqrQ';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: []
+    }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
   render() {
     return (
-      <div>React app</div>
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
+
+export default App;
