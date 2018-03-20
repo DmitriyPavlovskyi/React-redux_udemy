a70bd794d5db2b9e7283f14e6a8bfef015c0ca75
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-20 11:09:41 +0200

    youtube api key added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createStore, applyMiddleware } from 'redux';
 import Root from './root/app';
 import reducers from './root/reducer';
 
+const API_KEY = 'AIzaSyD31iFmLH-V4BVq8EDTRH8rY9Xd511eqrQ';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
