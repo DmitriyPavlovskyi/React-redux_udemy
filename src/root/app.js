fa8f6b6f849ea318c301f7ae5d7f49aa8da24608
dmitriypavlovskyi <pavlovskyidmitriy@gmail.com>
2018-03-20 15:26:00 +0200

    Video details and selection added

--- src/root/app.js
+++ src/root/app.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from '../features/search_bar/index';
+import VideoDetail from '../features/video_detail/index';
 import VideoList from '../features/video_list/index';
 
 const API_KEY = 'AIzaSyD31iFmLH-V4BVq8EDTRH8rY9Xd511eqrQ';
@@ -11,11 +12,15 @@ class App extends Component {
     super(props);
 
     this.state = {
-      videos: []
+      videos: [],
+      selectedVideo: null
     }
 
     YTSearch({key: API_KEY, term: 'infinity war'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -23,7 +28,11 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          videos={this.state.videos}
+          onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+        />
       </div>
     );
   }
